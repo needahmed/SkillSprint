36f877f448a173cef1c607a66f1348bb00e69c29
needahmed <needahmed2@gmail.com>
2024-05-19 01:06:24 +0500

    Updated next.config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    API_BASE_URL:
+      process.env.API_BASE_URL ||
+      "https://skillsprint-backend-0v8m.onrender.com",
+  },
+};
 
 export default nextConfig;
